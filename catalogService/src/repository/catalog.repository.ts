313c945c07528f7fcfd6fddabf8396dc22b63a94
Yusufjenom <jenomy32@gmail.com>
2024-03-02 02:54:31 -0800

    [Backend]build: created the catalog repo using OOP

---
+++ catalogService/src/repository/catalog.repository.ts
@@ -0,0 +1,20 @@
+import { IcatalogRepository } from "../interface/catalog.repository.interface";
+import { Product } from "../models/product";
+
+export class CatalogRepository implements IcatalogRepository{
+    create(data: Product): Promise<Product> {
+        throw new Error("Method not implemented.");
+    }
+    update(data: Product): Promise<Product> {
+        throw new Error("Method not implemented.");
+    }
+    delete(id: any) {
+        throw new Error("Method not implemented.");
+    }
+    find(): Promise<Product[]> {
+        throw new Error("Method not implemented.");
+    }
+    findOne(id: number): Promise<Product> {
+        throw new Error("Method not implemented.");
+    }
+};
