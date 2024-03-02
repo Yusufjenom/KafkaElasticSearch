cddef48c19894e699b2af05e30909cbc58889ef9
Yusufjenom <jenomy32@gmail.com>
2024-03-02 02:51:23 -0800

    [Backend]build: created the catalogservice with a bit of data encapsulation

---
+++ catalogService/src/services/catalog.service.ts
@@ -0,0 +1,23 @@
+import { IcatalogRepository } from "../interface/catalog.repository.interface";
+
+export class CatalogService{
+    private _repository: IcatalogRepository
+    constructor(repository: IcatalogRepository){
+        this._repository = repository
+    }
+    createProduct(input: any){
+
+    }
+    updateProduct(input: any){
+
+    }
+    getProducts(limit: number, offset: number){
+
+    }
+    getProduct(id: number){
+
+    }
+    deleteProduct(id: number){
+
+    }
+}
