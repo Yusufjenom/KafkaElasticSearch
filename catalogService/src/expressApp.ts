91a2414d7540f40f2de94ec236d16cfd759d74f2
Yusufjenom <jenomy32@gmail.com>
2024-03-02 03:28:14 -0800

    [Backend]refactor: initialised the expressApp file to sync with the server file

--- catalogService/src/expressApp.ts
+++ catalogService/src/expressApp.ts
@@ -1,7 +1,13 @@
 import express from 'express';
+import catalogRouter from './api/catalog.routes';
 
-const port = process.env.PORT || 8080;
+//const port = process.env.PORT || 8080;
 
 const app = express();
+app.use(express.json());
 
-app.listen(port, () => console.log(`server running on port ${port}`));
+app.use('/', catalogRouter);
+
+
+
+export default app;
